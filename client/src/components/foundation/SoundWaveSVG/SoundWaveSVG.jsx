7c4509b21f7fecd13b78f67d5793d4f80b677eb9
Leko <leko.noor@gmail.com>
2021-12-06 03:31:07 +0800

    perf: remove redundant logic

--- client/src/components/foundation/SoundWaveSVG/SoundWaveSVG.jsx
+++ client/src/components/foundation/SoundWaveSVG/SoundWaveSVG.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
 const chunk = (arr, chunkSize = 1, cache = []) => {
-  const tmp = [...arr]
-  if (chunkSize <= 0) return cache
-  while (tmp.length) cache.push(tmp.splice(0, chunkSize))
-  return cache
-}
+  const tmp = [...arr];
+  if (chunkSize <= 0) return cache;
+  while (tmp.length) cache.push(tmp.splice(0, chunkSize));
+  return cache;
+};
 
 /**
  * @param {ArrayBuffer} data
@@ -17,7 +17,7 @@ async function calculate(data) {
   // 音声をデコードする
   /** @type {AudioBuffer} */
   const buffer = await new Promise((resolve, reject) => {
-    audioCtx.decodeAudioData(data.slice(0), resolve, reject);
+    audioCtx.decodeAudioData(data, resolve, reject);
   });
   // 左の音声データの絶対値を取る
   const leftData = buffer.getChannelData(0).map(Math.abs);
@@ -29,7 +29,7 @@ async function calculate(data) {
   // 100 個の chunk に分ける
   const chunks = chunk(normalized, Math.ceil(normalized.length / 100));
   // chunk ごとに平均を取る
-  const peaks = chunks.map(c => c.reduce((a, b) => a+b) / c.length);
+  const peaks = chunks.map((c) => c.reduce((a, b) => a + b) / c.length);
   // chunk の平均の中から最大値を取る
   const max = Math.max(...peaks);
 
@@ -45,7 +45,6 @@ async function calculate(data) {
  * @type {React.VFC<Props>}
  */
 const SoundWaveSVG = ({ soundData }) => {
-  const uniqueIdRef = React.useRef(Math.random().toString(16));
   const [{ max, peaks }, setPeaks] = React.useState({ max: 0, peaks: [] });
 
   React.useEffect(() => {
@@ -58,9 +57,7 @@ const SoundWaveSVG = ({ soundData }) => {
     <svg className="w-full h-full" preserveAspectRatio="none" viewBox="0 0 100 1">
       {peaks.map((peak, idx) => {
         const ratio = peak / max;
-        return (
-          <rect key={`${uniqueIdRef.current}#${idx}`} fill="#2563EB" height={ratio} width="1" x={idx} y={1 - ratio} />
-        );
+        return <rect key={idx} fill="#2563EB" height={ratio} width="1" x={idx} y={1 - ratio} />;
       })}
     </svg>
   );
