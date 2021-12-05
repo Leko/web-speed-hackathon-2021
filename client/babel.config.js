55eafabfa787a2c6ef6c96a57dceacb5f0c15f5e
Leko <leko.noor@gmail.com>
2021-12-05 23:40:08 +0800

    perf: remove legacy settings

--- client/babel.config.js
+++ client/babel.config.js
@@ -1,18 +1,6 @@
 module.exports = {
   presets: [
-    [
-      '@babel/preset-env',
-      {
-        corejs: '3',
-        modules: 'commonjs',
-        useBuiltIns: false,
-      },
-    ],
-    [
-      '@babel/preset-react',
-      {
-        development: true,
-      },
-    ],
+    '@babel/preset-env',
+    '@babel/preset-react'
   ],
 };
