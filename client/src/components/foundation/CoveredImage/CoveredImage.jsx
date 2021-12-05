ada7e3cc50d2e613ea2e2e96721a7f8628635616
Leko <leko.noor@gmail.com>
2021-12-06 01:51:12 +0800

    perf: remove ProviderPlugin

--- client/src/components/foundation/CoveredImage/CoveredImage.jsx
+++ client/src/components/foundation/CoveredImage/CoveredImage.jsx
@@ -1,9 +1,5 @@
 import classNames from 'classnames';
-import sizeOf from 'image-size';
-import React from 'react';
-
-import { useFetch } from '../../../hooks/use_fetch';
-import { fetchBinary } from '../../../utils/fetchers';
+import React, { useRef } from 'react';
 
 /**
  * @typedef {object} Props
@@ -16,41 +12,36 @@ import { fetchBinary } from '../../../utils/fetchers';
  * @type {React.VFC<Props>}
  */
 const CoveredImage = ({ alt, src }) => {
-  const { data, isLoading } = useFetch(src, fetchBinary);
-
-  const imageSize = React.useMemo(() => {
-    return data !== null ? sizeOf(Buffer.from(data)) : null;
-  }, [data]);
-
-  const blobUrl = React.useMemo(() => {
-    return data !== null ? URL.createObjectURL(new Blob([data])) : null;
-  }, [data]);
+  /**
+   * @type {React.MutableRefObject<HTMLImageElement>}
+   */
+  const imgRef = useRef(null)
 
   const [containerSize, setContainerSize] = React.useState({ height: 0, width: 0 });
   /** @type {React.RefCallback<HTMLDivElement>} */
   const callbackRef = React.useCallback((el) => {
+    if (!imgRef.current) {
+      return
+    }
     setContainerSize({
       height: el?.clientHeight ?? 0,
       width: el?.clientWidth ?? 0,
     });
   }, []);
 
-  if (isLoading || data === null || blobUrl === null) {
-    return null;
-  }
-
   const containerRatio = containerSize.height / containerSize.width;
-  const imageRatio = imageSize?.height / imageSize?.width;
+  const imageRatio = imgRef.current?.naturalHeight / imgRef.current?.naturalWidth;
 
   return (
     <div ref={callbackRef} className="relative w-full h-full overflow-hidden">
       <img
+        ref={imgRef}
         alt={alt}
         className={classNames('absolute left-1/2 top-1/2 max-w-none transform -translate-x-1/2 -translate-y-1/2', {
           'w-auto h-full': containerRatio > imageRatio,
           'w-full h-auto': containerRatio <= imageRatio,
         })}
-        src={blobUrl}
+        src={src}
         loading="lazy"
       />
     </div>
