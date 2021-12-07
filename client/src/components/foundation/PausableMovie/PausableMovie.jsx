ebd38ab88f255a0ae71f0d2a56ad628d48abada2
Leko <leko.noor@gmail.com>
2021-12-07 19:15:38 +0800

    perf: disable autoPlay when the device perfers reduced motion

--- client/src/components/foundation/PausableMovie/PausableMovie.jsx
+++ client/src/components/foundation/PausableMovie/PausableMovie.jsx
@@ -4,6 +4,8 @@ import React, { useRef } from 'react';
 import { AspectRatioBox } from '../AspectRatioBox';
 import { FontAwesomeIcon } from '../FontAwesomeIcon';
 
+const prefersReducedMotion = window.matchMedia('(prefers-reduced-motion: reduce)').matches;
+
 /**
  * @typedef {object} Props
  * @property {string} src
@@ -19,7 +21,7 @@ const PausableMovie = ({ src }) => {
    */
   const videoRef = useRef(null);
 
-  const [isPlaying, setIsPlaying] = React.useState(true);
+  const [isPlaying, setIsPlaying] = React.useState(!prefersReducedMotion);
   const handleClick = React.useCallback(() => {
     setIsPlaying((isPlaying) => {
       if (isPlaying) {
@@ -34,7 +36,7 @@ const PausableMovie = ({ src }) => {
   return (
     <AspectRatioBox aspectHeight={1} aspectWidth={1}>
       <button className="group relative block w-full h-full" onClick={handleClick} type="button">
-        <video ref={videoRef} src={src} className="w-full" muted autoPlay loop />
+        <video ref={videoRef} src={src} className="w-full" muted autoPlay={!prefersReducedMotion} loop />
         <div
           className={classNames(
             'absolute left-1/2 top-1/2 flex items-center justify-center w-16 h-16 text-white text-3xl bg-black bg-opacity-50 rounded-full transform -translate-x-1/2 -translate-y-1/2',
