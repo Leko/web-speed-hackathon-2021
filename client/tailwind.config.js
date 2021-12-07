33b91b0d55e1e6af86972cf446449a52300fd7eb
Leko <leko.noor@gmail.com>
2021-12-07 22:42:50 +0800

    perf: purge unused keyframes and fontFaces

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,7 +1,19 @@
 module.exports = {
   darkMode: false,
-  purge: [
-    './src/**/*.html',
-    './src/**/*.jsx',
-  ],
+  purge: {
+    content: ['./src/**/*.html', './src/**/*.jsx'],
+    options: {
+      keyframes: true,
+      fontFace: true,
+    },
+  },
 };
+
+// No options:
+// -rw-r--r--  1 leko  staff  391167 Dec  7 21:14 ../dist/styles/main.css
+
+// keyframes=true
+// -rw-r--r--  1 leko  staff  390405 Dec  7 22:40 ../dist/styles/main.css
+
+// fontFace=true
+// -rw-r--r--  1 leko  staff  12148 Dec  7 22:41 ../dist/styles/main.css
