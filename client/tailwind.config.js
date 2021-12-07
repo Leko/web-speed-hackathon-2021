a2cfa78a6bd7e206d4a45117a80fa481f03fd7e5
Leko <leko.noor@gmail.com>
2021-12-07 22:51:40 +0800

    Revert "perf: purge unused keyframes and fontFaces"
    This reverts commit 6c20db91d38ac8690d10eb1bfa578d0cfd7fed54.

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,19 +1,7 @@
 module.exports = {
   darkMode: false,
-  purge: {
-    content: ['./src/**/*.html', './src/**/*.jsx'],
-    options: {
-      keyframes: true,
-      fontFace: true,
-    },
-  },
+  purge: [
+    './src/**/*.html',
+    './src/**/*.jsx',
+  ],
 };
-
-// No options:
-// -rw-r--r--  1 leko  staff  391167 Dec  7 21:14 ../dist/styles/main.css
-
-// keyframes=true
-// -rw-r--r--  1 leko  staff  390405 Dec  7 22:40 ../dist/styles/main.css
-
-// fontFace=true
-// -rw-r--r--  1 leko  staff  12148 Dec  7 22:41 ../dist/styles/main.css
