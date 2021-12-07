4e8432fb900dcb0eeceb7e6627d7666ba2141a63
Leko <leko.noor@gmail.com>
2021-12-08 01:19:08 +0800

    fix: enable sslmode

--- server/src/sequelize.js
+++ server/src/sequelize.js
@@ -1,5 +1,9 @@
 import { Sequelize } from 'sequelize';
 
-const sequelize = new Sequelize(process.env.DATABASE_URL ?? 'sqlite::memory:', { logging: false });
+const sequelize = new Sequelize(
+  process.env.DATABASE_URL ? `${process.env.DATABASE_URL}?sslmode=require` : 'sqlite::memory:',
+  { logging: false },
+);
+sequelize;
 
 export { sequelize };
