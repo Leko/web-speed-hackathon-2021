6e5e7808baba1488f16488011eb47f8b40a1c12f
Leko <leko.noor@gmail.com>
2021-12-08 09:17:14 +0800

    fix: use DELETE instead

--- server/src/seeds.js
+++ server/src/seeds.js
@@ -10,14 +10,14 @@ import users from '../seeds/users.json';
 import { Comment, Image, Movie, Post, PostsImagesRelation, ProfileImage, Sound, User } from './models';
 
 async function insertSeeds() {
-  await Comment.destroy({ truncate: true, logging: false });
-  await PostsImagesRelation.destroy({ truncate: true, logging: false });
-  await Post.destroy({ truncate: true, logging: false });
-  await User.destroy({ truncate: true, logging: false });
-  await Sound.destroy({ truncate: true, logging: false });
-  await Movie.destroy({ truncate: true, logging: false });
-  await Image.destroy({ truncate: true, logging: false });
-  await ProfileImage.destroy({ truncate: true, logging: false });
+  await Comment.destroy({ where: { 1: '1' }, logging: false });
+  await PostsImagesRelation.destroy({ where: { 1: '1' }, logging: false });
+  await Post.destroy({ where: { 1: '1' }, logging: false });
+  await User.destroy({ where: { 1: '1' }, logging: false });
+  await Sound.destroy({ where: { 1: '1' }, logging: false });
+  await Movie.destroy({ where: { 1: '1' }, logging: false });
+  await Image.destroy({ where: { 1: '1' }, logging: false });
+  await ProfileImage.destroy({ where: { 1: '1' }, logging: false });
 
   await ProfileImage.bulkCreate(profileImages, { logging: false });
   await Image.bulkCreate(images, { logging: false });
