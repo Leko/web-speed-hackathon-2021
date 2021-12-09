fb7f5ae77a38b6644ecba8971dcf343335202698
Leko <leko.noor@gmail.com>
2021-12-10 00:46:36 +0800

    refactor: migrate to postgres

--- server/src/sequelize.js
+++ server/src/sequelize.js
@@ -1,17 +1,14 @@
 import { Sequelize } from 'sequelize';
 
-const sequelize = new Sequelize('sqlite::memory:', {
+const sequelize = new Sequelize(process.env.DATABASE_URL ?? 'sqlite::memory:', {
   logging: false,
+  dialectOptions: process.env.DATABASE_URL
+    ? {
+        ssl: {
+          rejectUnauthorized: false,
+        },
+      }
+    : {},
 });
-// const sequelize = new Sequelize(process.env.DATABASE_URL ?? 'sqlite::memory:', {
-//   logging: false,
-//   dialectOptions: process.env.DATABASE_URL
-//     ? {
-//         ssl: {
-//           rejectUnauthorized: false,
-//         },
-//       }
-//     : {},
-// });
 
 export { sequelize };
